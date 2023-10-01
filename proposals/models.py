88c45ad9da669c2c5fd27ed5deae3c49ae3607e0
Dias <uaudias@gmail.com>
2023-10-01 23:25:09 +0600

    final

--- proposals/models.py
+++ proposals/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Proposes(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Имя")
+    contact = models.CharField(max_length=50, verbose_name="Способ связаться!") # Здесь может быть связи по типу ForeignKey по пользователям!
+    title = models.CharField(max_length=50, verbose_name="Тема")
+    description = models.TextField(max_length=255, verbose_name="Описание")
+    date_created = models.DateTimeField(default=timezone.now())
+
+    class Meta:
+        app_label = "proposals"
+        ordering = ["-date_created", "name", "title"]
+        verbose_name = "Предложения или жалоба"
+        verbose_name_plural = "Предложении или жалобы"
+
+    def __str__(self):
+        return f"Предложения или жалоба от {self.name} по тему: {self.title}"
--- proposals/views.py
+++ proposals/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from proposals import models
+from django.db import connection
 
 # Create your views here.
+def homepage(request):
+    if request.method == 'GET':
+        return render(request, 'homepage.html')
+    elif request.method == 'POST':
+
+        name = request.POST.get('name')
+        contact = request.POST.get('contact')
+        title = request.POST.get('title')
+        description = request.POST.get('desc')
+        switch = request.POST.get('switch')
+
+        if name == '':
+            return render(request, "homepage.html", context={"error": "Ошибка!"})
+
+        context = {"message": "Ваше предложение(жалоба) принято! Пожалуйста ждите обратной связи!"}
+
+        if switch is None:
+            props = models.Proposes.objects.create(name=name, contact=contact, title=title, description=description)
+        elif switch == 'on':
+            with connection.cursor() as cursor:
+                cursor.execute("INSERT INTO proposals_proposes (name, contact, title, description) VALUES (?, ?, ?, ?)", (name, contact, title, description))
+        else:
+            context = {"error": "Ошибка!"}
+
+        return render(request, "homepage.html", context=context)
